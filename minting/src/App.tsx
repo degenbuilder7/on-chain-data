c07d86e5480adb5776fdb31d60c0e6b4aaa81306
kamalishere <kamaleinstein1@gmail.com>
2023-06-14 10:50:20 +0000

    minting done 🚀

--- minting/src/App.tsx
+++ minting/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Header from "./Components/Header";
+import Initialize from "./Components/Initialize";
+import { useState } from "react";
+import { Container } from "@mui/material";
+import Mint from "./Components/Mint";
+
+export default function App() {
+  const [isConnected, setConnected] = useState(false);
+ return (
+  <div className="App">
+   <Header
+        onConnected={() => setConnected(true)}
+        onDisconnected={() => setConnected(false)}
+   />
+      
+      <Container sx={{ mt: 15 }}>
+        {!isConnected && <Initialize/>}
+        {!isConnected && <Mint/>}
+   </Container>
+
+  </div>
+ );
 }
 
-export default App;
+
+
